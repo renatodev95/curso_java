f65fad790efb0dfa6a2af20431a67b0d225b5cdf
Renato <renatodev95@gmail.com>
2021-08-09 08:31:17 -0300

    Filter

--- curso_java/src/streams/Filter.java
+++ curso_java/src/streams/Filter.java
@@ -9,12 +9,12 @@ public class Filter {
 
 	public static void main(String[] args) {
 
-		Aluno a1 = new Aluno("Ana", 7.8);
-		Aluno a2 = new Aluno("Bia", 5.8);
-		Aluno a3 = new Aluno("Daniel", 9.8);
-		Aluno a4 = new Aluno("Gui", 6.8);
-		Aluno a5 = new Aluno("Rebeca", 7.1);
-		Aluno a6 = new Aluno("Pedro", 8.8);
+		Aluno a1 = new Aluno("Ana", 7.8, true);
+		Aluno a2 = new Aluno("Bia", 5.8, false);
+		Aluno a3 = new Aluno("Daniel", 9.8, true);
+		Aluno a4 = new Aluno("Gui", 6.8, true);
+		Aluno a5 = new Aluno("Rebeca", 7.1, false);
+		Aluno a6 = new Aluno("Pedro", 8.8, true);
 
 		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
 		
