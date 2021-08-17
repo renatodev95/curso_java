b55604e79a02e650000ca72b821c7179f414c7ea
Renato <renatodev95@gmail.com>
2021-08-17 08:02:03 -0300

    Generics e Heranca

---
+++ curso_java/src/generics/CaixaNumero.java
@@ -0,0 +1,8 @@
+package generics;
+
+public class CaixaNumero<N extends Number> extends Caixa<N> {
+	// Criando restrições com relação aos tipos que CaixaNumero
+	// poderá conter, N está herdando da classe Number, dessa forma,
+	// o tipo resolvido em Caixa deverá ser o mesmo que está herdando
+	// de Number, no caso o N.
+}
---
+++ curso_java/src/generics/CaixaNumeroTeste.java
@@ -0,0 +1,15 @@
+package generics;
+
+public class CaixaNumeroTeste {
+
+	public static void main(String[] args) {
+		
+		CaixaNumero<Double> caixaA = new CaixaNumero<>();
+		caixaA.guardar(5.5);
+		System.out.println(caixaA.abrir());
+		
+		CaixaNumero<Integer> caixaB = new CaixaNumero<>();
+		caixaB.guardar(125);
+		System.out.println(caixaB.abrir());
+	}
+}
