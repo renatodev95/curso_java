9c454e0b988cbaecc765308ff0424b9eb22daee2
Renato Pereira Santos <renatodev95@gmail.com>
2021-08-19 08:42:00 -0300

    criando uma Factory Connection para acesso ao banco

---
+++ curso_java/src/jdbc/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	// criando uma Fábrica de Conexões para reaproveitamento de código
+	public static Connection getConnection() {
+		try {
+			String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+			String usuario = "root";
+			String senha = "12345678";
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
